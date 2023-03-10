b161cd92b78779e6d3330e067f4d461e32299ee0
AkaMirai9 <105291924+AkaMirai9@users.noreply.github.com>
2023-03-10 15:17:25 +0100

    Update main.go

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func main() {
 	r := gin.Default()
 
 	r.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{"data": "Hello Gaia"})
+		c.JSON(http.StatusOK, gin.H{"data": "Hello Ayman Imhah"})
 	})
 
 	r.Run()
