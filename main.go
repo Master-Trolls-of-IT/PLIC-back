3efc4821d304e5be45a6651fe3f2a98faab0bbdb
Maxner Demilon <124256635+AymanImhah@users.noreply.github.com>
2023-03-18 22:47:51 +0100

    connecting cloud run to api attempt

--- main.go
+++ main.go
@@ -5,37 +5,75 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"reflect"
+	"os"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/lib/pq"
 )
 
-func main() {
-    connStr := "host=localhost port=1234 dbname=gaia user=admin password=gaia2024 sslmode=disable"
-
-	db, err := sql.Open("postgres", connStr)
-	if err != nil {
-		panic(err.Error())
+func connectUnixSocket() (*sql.DB, error) {
+	mustGetenv := func(k string) string {
+		v := os.Getenv(k)
+		if v == "" {
+			log.Fatalf("Fatal Error in connect_unix.go: %s environment variable not set.", k)
+		}
+		return v
 	}
-	defer db.Close()
+	// Note: Saving credentials in environment variables is convenient, but not
+	// secure - consider a more secure solution such as
+	// Cloud Secret Manager (https://cloud.google.com/secret-manager) to help
+	// keep secrets safe.
+	var (
+		dbUser         = mustGetenv("admin")                                          // e.g. 'my-db-user'
+		dbPwd          = mustGetenv("gaia2024")                                       // e.g. 'my-db-password'
+		dbName         = mustGetenv("gaia")                                           // e.g. 'my-database'
+		unixSocketPath = mustGetenv("/cloudsql/gaia-api-380213:europe-west9:gaia-db") // e.g. '/cloudsql/project:region:instance'
+	)
 
-    err = db.Ping()
-	fmt.Println("Connected to Google Cloud SQL instance Postgres database!")
+	dbURI := fmt.Sprintf("%s:%s@unix(%s)/%s?parseTime=true",
+		dbUser, dbPwd, unixSocketPath, dbName)
 
-    if err != nil {
-        panic(err)
-    }
-	stmt , err := db.Query("SELECT tablename FROM pg_catalog.pg_tables;")
-	
-	if err !=nil {
+	// dbPool is the pool of database connections.
+	dbPool, err := sql.Open("postgres", dbURI)
+	if err != nil {
+		return nil, fmt.Errorf("sql.Open: %v", err)
+	}
+
+	// ...
+
+	return dbPool, nil
+}
+
+func main() {
+	/*connStr := "host= port= dbname=gaia user=admin password=gaia2024 sslmode=disable"
+
+		db, err := sql.Open("postgres", connStr)
+		if err != nil {
+			panic(err.Error())
+		}
+		defer db.Close()
+
+	    err = db.Ping()
+
+	    if err != nil {
+	        panic(err)
+	    }
+
+		fmt.Println("Connected to Google Cloud SQL instance Postgres database!")
+	*/
+	db, err := connectUnixSocket()
+	if err != nil {
+		panic(err)
+	}
+
+	stmt, err := db.Query("SELECT tablename FROM pg_catalog.pg_tables;")
+
+	if err != nil {
 		stmt.Err()
 	}
 
-	
-	fmt.Println(reflect.TypeOf(stmt))
-	var tables []string;
-	var name string;
+	var tables []string
+	var name string
 	for stmt.Next() {
 		err := stmt.Scan(&name)
 		if err != nil {
@@ -45,13 +83,10 @@ func main() {
 	}
 	log.Println(tables)
 
-
-
-
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"data": tables})
-	})	
+	})
 
 	r.Run()
 }
