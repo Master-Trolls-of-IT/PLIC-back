5612fd0105268f5d40ee6ca9d2f85e5958b9a7fc
Henry Sargerson <91054494+lesageduvillage@users.noreply.github.com>
2023-06-19 16:52:56 +0200

    testing a push with a failed build

--- infrastructure/controllers/api_test.go
+++ infrastructure/controllers/api_test.go
@@ -77,6 +77,7 @@ func TestRegisterUser(t *testing.T) {
 	}`
 
 	user := entities.User{}
+	user := entities.User{}
 	invalidUser := entities.User{}
 	//Bind the JSON data to the entities.User struct
 	err := json.Unmarshal([]byte(validUserJSON), &user)
