e2f411b27daea4ccb35ab5901f5eb9c3b7b5cd82
Maxner Demilon <124256635+AymanImhah@users.noreply.github.com>
2023-06-19 17:15:16 +0200

    refacto

--- infrastructure/repository/user_repository.go
+++ infrastructure/repository/user_repository.go
@@ -22,7 +22,8 @@ func (userRepo *UserRepo) getUser(query string, args ...interface{}) (entity.Use
 		return entity.User{}, err
 	}
 	var user entity.User
-	err = stmt.QueryRow(args...).Scan(&user.Id, &user.Rights, &user.Email, &user.Username, &user.Birthdate, &user.Weight, &user.Height, &user.Gender, &user.Sportiveness, &user.BasalMetabolism, &user.Password, &user.Pseudo)
+	err = stmt.QueryRow(args...).Scan(&user.Id, &user.Rights, &user.Email, &user.Username, &user.Birthdate, &user.Weight,
+		&user.Height, &user.Gender, &user.Sportiveness, &user.BasalMetabolism, &user.Password, &user.Pseudo)
 	if err != nil {
 		return entity.User{}, err
 	}
@@ -51,15 +52,18 @@ func (userRepo *UserRepo) CheckLogin(loginInfo *entity.Login_info) (bool, error)
 func (userRepo *UserRepo) Register(userInfo *entity.User) (bool, error) {
 	var db = userRepo.data.DB
 	var count int
-	err := db.QueryRow("SELECT COUNT(*) FROM users WHERE username=$1 OR email=$2", userInfo.Username, userInfo.Email).Scan(&count)
+	err := db.QueryRow("SELECT COUNT(*) FROM users WHERE username=$1 OR email=$2", userInfo.Username,
+		userInfo.Email).Scan(&count)
 	if err != nil {
 		return false, err
 	}
 	if count > 0 {
 		return false, nil
 	}
-	_, err = db.Exec("INSERT INTO users (email, pseudo, rights,username, password, birthdate, weight, height, gender, sportiveness, basalmetabolism) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)",
-		userInfo.Email, userInfo.Pseudo, userInfo.Rights, userInfo.Username, userInfo.Password, userInfo.Birthdate, userInfo.Weight, userInfo.Height, userInfo.Gender, userInfo.Sportiveness, userInfo.BasalMetabolism)
+	_, err = db.Exec("INSERT INTO users (email, pseudo, rights,username, password, birthdate, weight, height, "+
+		"gender, sportiveness, basalmetabolism) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)",
+		userInfo.Email, userInfo.Pseudo, userInfo.Rights, userInfo.Username, userInfo.Password, userInfo.Birthdate,
+		userInfo.Weight, userInfo.Height, userInfo.Gender, userInfo.Sportiveness, userInfo.BasalMetabolism)
 	if err != nil {
 		return false, err
 	}
