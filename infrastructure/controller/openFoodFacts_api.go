7994d808008a999e92057ca38de0f0b98ee98b60
Ayman Imhah <aymanimhah@Aymans-MacBook-Air.local>
2023-10-08 20:48:37 +0200

    Adding:  nutrients_serving, nutrients_value, nutrients_100g, quantity, serving_quantity, serving_size data

--- infrastructure/controller/openFoodFacts_api.go
+++ infrastructure/controller/openFoodFacts_api.go
@@ -35,32 +35,94 @@ func (openFoodFactsAPI *OpenFoodFactsAPI) retrieveAndMapProduct(barcode string) 
 
 func mapOpenFoodFactsProductToEntitiesProduct(product *openfoodfacts.Product) (entity.Product, error) {
 	nutrients := product.Nutriments
-	mappedNutriscore := entity.NutriScore{Score: product.Nutriments.NutritionScoreFr100G, Grade: product.NutritionGradeFr}
 	isWater := false
 	if slices.Contains(product.CategoriesTags, "en:waters") {
 		isWater = true
 	}
 
+	mappedNutriscore := entity.NutriScore{Score: product.Nutriments.NutritionScoreFr100G, Grade: product.NutritionGradeFr}
+
 	mappedNutrients := entity.Nutrients{
-		EnergyKj:      nutrients.Energy100G,
-		EnergyKcal:    nutrients.EnergyKcal100G,
-		Fat:           nutrients.Fat100G,
-		SaturatedFat:  nutrients.SaturatedFat100G,
-		Carbohydrates: nutrients.Carbohydrates100G,
-		Sugar:         nutrients.Sugars100G,
-		Fiber:         nutrients.Fiber100G,
-		Proteins:      nutrients.Proteins100G,
-		Salt:          nutrients.Salt100G,
+		EnergyKj:      nutrients.Energy,
+		EnergyKcal:    nutrients.EnergyKcal,
+		Fat:           nutrients.Fat,
+		SaturatedFat:  nutrients.SaturatedFat,
+		Carbohydrates: nutrients.Carbohydrates,
+		Sugar:         nutrients.Sugars,
+		Fiber:         nutrients.Fiber,
+		Proteins:      nutrients.Proteins,
+		Salt:          nutrients.Salt,
 	}
 
+	mappedNutrients100g := entity.Nutrients100g{
+		entity.Nutrients{
+			EnergyKj:      nutrients.Energy100G,
+			EnergyKcal:    nutrients.EnergyKcal100G,
+			Fat:           nutrients.Fat100G,
+			SaturatedFat:  nutrients.SaturatedFat100G,
+			Carbohydrates: nutrients.Carbohydrates100G,
+			Sugar:         nutrients.Sugars100G,
+			Fiber:         nutrients.Fiber100G,
+			Proteins:      nutrients.Proteins100G,
+			Salt:          nutrients.Salt100G,
+		},
+	}
+
+	mappedNutrientsValue := entity.NutrientsValue{
+		entity.Nutrients{
+			EnergyKj:      nutrients.EnergyValue,
+			EnergyKcal:    nutrients.EnergyKcalValue,
+			Fat:           nutrients.FatValue,
+			SaturatedFat:  nutrients.SaturatedFatValue,
+			Carbohydrates: nutrients.CarbohydratesValue,
+			Sugar:         nutrients.SugarsValue,
+			Fiber:         nutrients.FiberValue,
+			Proteins:      nutrients.ProteinsValue,
+			Salt:          nutrients.SaltValue,
+		},
+	}
+
+	mappedNutrientsServing := entity.NutrientsServing{
+		entity.Nutrients{
+			EnergyKj:      nutrients.EnergyServing,
+			EnergyKcal:    nutrients.EnergyKcalServing,
+			Fat:           nutrients.FatServing,
+			SaturatedFat:  nutrients.SaturatedFatServing,
+			Carbohydrates: nutrients.CarbohydratesServing,
+			Sugar:         nutrients.SugarsServing,
+			Fiber:         nutrients.FiberServing,
+			Proteins:      nutrients.ProteinsServing,
+			Salt:          nutrients.SaltServing,
+		},
+	}
+
+	//mappedNutrientsUnit := entity.NutrientsUnit{
+	//	EnergyKj:      nutrients.EnergyUnit,
+	//	EnergyKcal:    nutrients.EnergyKcalUnit,
+	//	Fat:           nutrients.FatUnit,
+	//	SaturatedFat:  nutrients.SaturatedFatUnit,
+	//	Carbohydrates: nutrients.CarbohydratesUnit,
+	//	Sugar:         nutrients.SugarsUnit,
+	//	Fiber:         nutrients.FiberUnit,
+	//	Proteins:      nutrients.ProteinsUnit,
+	//	Salt:          nutrients.SaltUnit,
+	//}
+
 	mappedProduct := entity.Product{
-		Brand:      product.Brands,
-		Name:       product.ProductName,
-		Nutrients:  mappedNutrients,
-		ImageURL:   product.ImageFrontURL.URL.String(),
-		NutriScore: mappedNutriscore,
-		EcoScore:   product.EcoscoreGrade,
-		IsWater:    isWater,
+		Brand:            product.Brands,
+		Name:             product.ProductName,
+		Nutrients:        mappedNutrients,
+		Nutrients100g:    mappedNutrients100g,
+		NutrientsServing: mappedNutrientsServing,
+		NutrientsValue:   mappedNutrientsValue,
+		//NutrientsUnit:    mappedNutrientsUnit,
+		ImageURL:        product.ImageFrontURL.URL.String(),
+		NutriScore:      mappedNutriscore,
+		EcoScore:        product.EcoscoreGrade,
+		IsWater:         isWater,
+		Quantity:        product.Quantity,
+		ServingQuantity: product.ServingQuantity,
+		ServingSize:     product.ServingSize,
 	}
 
 	return mappedProduct, nil
--- infrastructure/repository/product_repository.go
+++ infrastructure/repository/product_repository.go
@@ -23,7 +23,7 @@ func (productRepo *ProductRepo) getProduct(query string, args ...interface{}) (e
 	err = stmt.QueryRow(args...).Scan(&product.ID, &product.Brand, &product.Name, &product.Nutrients.EnergyKj, &product.Nutrients.EnergyKcal,
 		&product.Nutrients.Fat, &product.Nutrients.SaturatedFat, &product.Nutrients.Sugar, &product.Nutrients.Fiber,
 		&product.Nutrients.Proteins, &product.Nutrients.Salt, &product.ImageURL, &product.NutriScore.Score,
-		&product.NutriScore.Grade, &product.IsWater)
+		&product.NutriScore.Grade, &product.IsWater, &product.Quantity, &product.ServingQuantity, &product.ServingSize)
 	if err != nil {
 		return entity.Product{}, err
 	}
@@ -81,28 +81,91 @@ func (productRepo *ProductRepo) getConsumedProducts(query string, args ...interf
 }
 
 func (productRepo *ProductRepo) GetProductByBarCode(barcode string) (entity.Product, error) {
-	return productRepo.getProduct("SELECT id, brand, name, energy_kj, energy_kcal, fat, saturated_fat, sugar, fiber, "+
-		"proteins, salt, image_url, nutriscore_score, nutriscore_grade, iswater FROM product WHERE barcode = $1", barcode)
+	product, err := productRepo.getProduct("SELECT id, brand, name, energy_kj, energy_kcal, fat, saturated_fat, sugar, fiber, "+
+		"proteins, salt, image_url, nutriscore_score, nutriscore_grade, iswater, quantity, serving_quantity, serving_size FROM product WHERE barcode = $1", barcode)
+
+	if err != nil {
+		return entity.Product{}, err
+	}
+
+	nutrients100g, err := productRepo.getNutrientsDataByProductID(product.ID, "nutrients_100g")
+	if err != nil {
+		return entity.Product{}, err
+	}
+
+	nutrientsValue, err := productRepo.getNutrientsDataByProductID(product.ID, "nutrients_value")
+	if err != nil {
+		return entity.Product{}, err
+	}
+
+	nutrientsServing, err := productRepo.getNutrientsDataByProductID(product.ID, "nutrients_serving")
+	if err != nil {
+		return entity.Product{}, err
+	}
+
+	product.Nutrients100g, product.NutrientsValue, product.NutrientsServing = entity.Nutrients100g{nutrients100g}, entity.NutrientsValue{nutrientsValue}, entity.NutrientsServing{nutrientsServing}
+
+	return product, nil
+}
+
+func (productRepo *ProductRepo) GetConsumedProductsByUserId(userID int) ([]entity.ConsumedProduct, error) {
+	query := "SELECT p.id, p.name ,p.brand, p.energy_kj, p.energy_kcal, p.fat, p.saturated_fat, p.sugar, p.fiber, p.proteins, p.salt, p.image_url, p.nutriscore_score, p.nutriscore_grade, cp.quantity FROM consumed_products cp INNER JOIN product p ON cp.product_id = p.id WHERE cp.user_id = $1"
+	return productRepo.getConsumedProducts(query, userID)
+}
+
+func (productRepo *ProductRepo) getNutrientsDataByProductID(productID int, table string) (entity.Nutrients, error) {
+	var db = productRepo.data.DB
+	var nutrients entity.Nutrients
+	query := "SELECT  energy_kj, energy_kcal, fat, saturated_fat, sugar, fiber, proteins, salt from " + table + " WHERE product_id = $1"
+	stmt, err := db.Prepare(query)
+	if err != nil {
+		return entity.Nutrients{}, err
+	}
+	err = stmt.QueryRow(productID).Scan(&nutrients.EnergyKj, &nutrients.EnergyKcal, &nutrients.Fat, &nutrients.SaturatedFat, &nutrients.Sugar, &nutrients.Fiber, &nutrients.Proteins, &nutrients.Salt)
+	if err != nil {
+		return entity.Nutrients{}, err
+	}
+
+	return nutrients, nil
 }
 
 func (productRepo *ProductRepo) SaveProduct(product entity.Product, barcode string) (bool, error) {
 	var database = productRepo.data.DB
-
-	_, err := database.Exec("INSERT INTO product (brand, name, energy_kj, energy_kcal, fat, saturated_fat, sugar,"+
-		" fiber, proteins, salt, image_url,  nutriscore_score, nutriscore_grade, barcode, iswater) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15) ",
+	var productID int
+	err := database.QueryRow("INSERT INTO product (brand, name, energy_kj, energy_kcal, fat, saturated_fat, sugar,"+
+		" fiber, proteins, salt, image_url,  nutriscore_score, nutriscore_grade, barcode, iswater, quantity, serving_quantity, serving_size) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18) RETURNING id",
 		product.Brand, product.Name, product.Nutrients.EnergyKj, product.Nutrients.EnergyKcal, product.Nutrients.Fat,
 		product.Nutrients.SaturatedFat, product.Nutrients.Sugar, product.Nutrients.Fiber, product.Nutrients.Proteins,
-		product.Nutrients.Salt, product.ImageURL, product.NutriScore.Score, product.NutriScore.Grade, barcode, product.IsWater)
+		product.Nutrients.Salt, product.ImageURL, product.NutriScore.Score, product.NutriScore.Grade, barcode, product.IsWater, product.Quantity, product.ServingQuantity, product.ServingSize).Scan(&productID) //, nutrientsUnitID)
+
 	if err != nil {
 		return false, err
 	}
+	err = productRepo.insertNutrientData(productID, "nutrients_100g", product.Nutrients100g.Nutrients)
+	if err != nil {
+		return false, err
+	}
+
+	err = productRepo.insertNutrientData(productID, "nutrients_Value", product.NutrientsValue.Nutrients)
+	if err != nil {
+		return false, err
+	}
+
+	err = productRepo.insertNutrientData(productID, "nutrients_Serving", product.NutrientsServing.Nutrients)
+	if err != nil {
+		return false, err
+	}
+
+	//nutrientsUnitID, err := productRepo.insertNutrientUnit(database, "nutrients_Unit", product.NutrientsUnit)
+	//if err != nil {
+	//	return false, err
+	//}
+
 	return true, nil
 }
 
 func (productRepo *ProductRepo) SaveConsumedProduct(product entity.Product, userID int, quantity int) (bool, error) {
-	// Prepare the insert statement
 	var database = productRepo.data.DB
-	// Execute the insert statement
 	_, err := database.Exec("INSERT INTO consumed_products (product_id, user_id, quantity, consumed_date)\n    VALUES ($1, $2, $3, $4)", product.ID, userID, quantity, time.Now().UTC().Format("2006-01-02"))
 
 	if err != nil {
@@ -113,9 +176,21 @@ func (productRepo *ProductRepo) SaveConsumedProduct(product entity.Product, user
 	return true, nil
 }
 
-func (productRepo *ProductRepo) GetConsumedProductsByUserId(userID int) ([]entity.ConsumedProduct, error) {
-	query := "SELECT p.id, p.name ,p.brand, p.energy_kj, p.energy_kcal, p.fat, p.saturated_fat, p.sugar, p.fiber, p.proteins, p.salt, p.image_url, p.nutriscore_score, p.nutriscore_grade, cp.quantity FROM consumed_products cp INNER JOIN product p ON cp.product_id = p.id WHERE cp.user_id = $1"
-	return productRepo.getConsumedProducts(query, userID)
+func (productRepo *ProductRepo) insertNutrientData(productID int, table string, nutrients entity.Nutrients) error {
+	var db = productRepo.data.DB
+
+	_, err := db.Exec(
+		"INSERT INTO "+table+" (energy_kj, energy_kcal, fat, saturated_fat, sugar, fiber, proteins, salt, product_id) "+
+			"VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)",
+		nutrients.EnergyKj, nutrients.EnergyKcal, nutrients.Fat,
+		nutrients.SaturatedFat, nutrients.Sugar, nutrients.Fiber, nutrients.Proteins,
+		nutrients.Salt, productID)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func (productRepo *ProductRepo) DeleteConsumedProduct(id int, userID int) (bool, error) {
@@ -126,3 +201,20 @@ func (productRepo *ProductRepo) DeleteConsumedProduct(id int, userID int) (bool,
 	}
 	return true, nil
 }
+
+//func (productRepo *ProductRepo) insertNutrientUnit(db *sql.DB, table string, nutrients entity.NutrientsUnit) (int64, error) {
+//	var id int64
+//	err := db.QueryRow(
+//		"INSERT INTO "+table+" (energy_kj, energy_kcal, fat, saturated_fat, sugar, fiber, proteins, salt) "+
+//			"VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING id",
+//		nutrients.EnergyKj, nutrients.EnergyKcal, nutrients.Fat,
+//		nutrients.SaturatedFat, nutrients.Sugar, nutrients.Fiber, nutrients.Proteins,
+//		nutrients.Salt,
+//	).Scan(&id)
+//
+//	if err != nil {
+//		return 0, err
+//	}
+//
+//	return id, nil
+//}
